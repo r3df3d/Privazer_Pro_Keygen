c6199bcd177545bc9e161c1146ef037a476c3618
RedFed <canikissyou121@proton.me>
2025-03-27 12:48:26 +0600

    Add files via upload

--- compile.bat
+++ compile.bat
@@ -1,5 +1,9 @@
 @echo off
-windres resources.rc -O coff -o resources.res
+echo Compiling resources...
+windres -i resources.rc -O coff -o resources.res
+
+echo Compiling application...
 g++ privazer_keygen.cpp resources.res -o privazer_keygen.exe -mwindows -lcomctl32 -luser32 -lgdi32
+
 echo Compilation complete.
 pause
